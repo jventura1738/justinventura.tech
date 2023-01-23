fd41caef0bbad7bafaa3f9a515d4555516fdb742
Justin Ventura <justinv9001@aol.com>
2023-01-23 16:45:44 -0500

    Format

--- justin-ventura-portfolio/src/App.js
+++ justin-ventura-portfolio/src/App.js
@@ -1,15 +1,13 @@
-import './css/App.css';
+import "./css/App.css";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Justin Ventura Portfolio
-        </p>
+        <p>Justin Ventura Portfolio 2</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
