ce6a4b36cf8c402b3f568b3b65bc54ae52cbc4f1
Justin Ventura <justinv9001@aol.com>
2023-01-23 16:30:29 -0500

    Initial commit

---
+++ justin-ventura-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import './css/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Justin Ventura Portfolio
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
