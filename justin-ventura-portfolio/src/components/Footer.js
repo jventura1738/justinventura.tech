b98e3cfb4ffcf7695fc6d6dd4d06e53f980c1713
Justin Ventura <justinv9001@aol.com>
2023-01-27 02:56:02 -0500

    Create footer

--- justin-ventura-portfolio/src/components/Footer.js
+++ justin-ventura-portfolio/src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  return <h1>Footer component.</h1>;
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex-1"></div>
+      <footer className="text-white bg-theme p-6">
+        <div className="container mx-auto">
+          <div className="flex justify-between items-center">
+            <p>Copyright © Justin Ventura {new Date().getFullYear()}</p>
+            <p>Created from scratch with React & Tailwind CSS</p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
