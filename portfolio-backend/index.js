5b12fd270f2bb376dd8bc382965d61a834899345
Justin Ventura <justinv9001@aol.com>
2023-01-31 22:41:33 -0500

    Contact backend added

---
+++ portfolio-backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log("Server started on http://localhost:3000");
+});
